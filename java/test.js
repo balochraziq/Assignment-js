1b48988162019f74da370cd2593e0cb4afd95cec
Raziq Ali <139381746+balochraziq@users.noreply.github.com>
2023-09-01 10:47:48 -0700

    java.Asmnt

---
+++ java/test.js
@@ -0,0 +1,13 @@
+// let a = + prompt("Enter yor age");
+ 
+// // if(a == 'girl'){
+// //     // equl
+// //     console.log(a);
+// // }
+// //if(a != 'girl'){
+//     // not eql
+//    // console.log(a);
+ 
+// if(a !== 18){
+//     console.log("your not Wellcome")
+// }
